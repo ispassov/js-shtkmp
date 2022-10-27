2ec2b7d41571ad61bae395bb1ef130feaa51fedb
ispassov <61962940+ispassov@users.noreply.github.com>
2022-10-27 16:17:05 +0300

    2 files modified, 1 added, 1 deleted

--- index.js
+++ index.js
@@ -1,63 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { initializeApp } from 'firebase/app'
-import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
-
-/* import * as bootstrap from 'bootstrap'
+import * as bootstrap from 'bootstrap'
 import Alert from 'bootstrap/js/dist/alert'
-import { Tooltip, Toast, Popover } from 'bootstrap' */
+import { Tooltip, Toast, Popover } from 'bootstrap'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAe6W7CWm0PMYW1CdN_MTs44qAm75Yg77s",
-  authDomain: "tasker-55cc6.firebaseapp.com",
-  projectId: "tasker-55cc6",
-  storageBucket: "tasker-55cc6.appspot.com",
-  messagingSenderId: "137378843685",
-  appId: "1:137378843685:web:543e8752ce71f0ef5f03cc",
-  measurementId: "G-9E50Q3S7WK"
-}
+let words = [
+  'After ', 'thorough ', 'deliberation ', 'Miss ', 'Universe ', 'Organization ', 'has ', 'decided ', 'to ', 'suspend ', 'Miss ', 'Brand ', 'immediately ', 'the ', 'Miss ', 'Universe ', 'Organization ', 'said ', 'in ', 'a ', 'statement ', 'to ', 'CNN. ', '“Miss ', 'Universe ', 'Organization ', 'will ', 'be ', 'taking ', 'over ', 'the ', 'Miss ', 'USA ', 'program ', 'while ', 'a ', 'comprehensive ', 'third-party ', 'investigation ', 'is ', 'conducted'
+]
+let btn = document.querySelectorAll('button')[0]
 
-const app = initializeApp(firebaseConfig)
-const auth = getAuth()
-
-let btnTest = document.querySelector('button')
-let btnLogin = document.querySelector('.login')
-let btnLogout = document.querySelector('.logout')
-let inputUsername = document.querySelector('.username')
-let inputPassword = document.querySelector('.password')
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    document.querySelectorAll('h1')[0].innerHTML = `Hello, ${user.email}!`
-  } else {
-    document.querySelectorAll('h1')[0].innerHTML = `Hello, Bootstrap and Webpack!`
-  }
-})
-
-btnTest.addEventListener('click', () => {
-  if(btnTest.innerHTML == 'Test') {
-    btnTest.innerHTML = '123'
-  }else {
-    btnTest.innerHTML = 'Test'
-  }
-})
-
-btnLogin.addEventListener('click', () => {
-  signInWithEmailAndPassword(auth, inputUsername.value, inputPassword.value)
-  .then((userCredential) => {
-  })
-  .catch((error) => {
-    if (error.message == 'Firebase: Error (auth/invalid-email).') {
-      document.querySelector('h1').innerHTML = 'Wrong email or password'
-    }
-  })
-})
-
-btnLogout.addEventListener('click', () => {
-  signOut(auth).then(() => {
-  }).catch((error) => {
-  })
-})
-
-inputUsername.addEventListener('click', () => {
-  document.querySelector('h1').innerHTML = 'Hello, Bootstrap and Webpack!'
+btn.addEventListener('click', () => {
+  btn.innerHTML = words[Math.floor((Math.random() * words.length))].toLowerCase()
 })
